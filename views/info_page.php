9a10dd265ed7dbf2af176d5003670a363f614434
MasterRO94 <igoshin18@gmail.com>
2014-10-27 04:10:07 +0200

    at least commit!

---
+++ views/info_page.php
@@ -0,0 +1,11 @@
+<?php defined('VIZITKI') or die('Access denied'); ?>
+
+<h1><?=$page['h1']?></h1>
+
+<div class="breadcrumps">
+    <a href="<?=PATH?>">Главная</a> / <?=$page['name_rus']?>
+</div>
+
+<section class="info_page">
+    <?=$page['text']?>
+</section>
